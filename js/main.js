bcef3266d244912a1903b2803f2d0d6ec37e3a9a
Fernando Saucedo <frsauce@gmail.com>
2023-08-23 00:37:37 -0300

    se agrego el tiempo y se comienza a trabajar con local storage

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+// recuperar el array de tareas del localStorage al cargar la página
+window.addEventListener('load', () => {
+  let tareaGuardada = localStorage.getItem('tareas');
+  if (tareaGuardada) {
+    arrayTareas = JSON.parse(tareaGuardada);
+  }
+});
+
 let ulNav = document.querySelector(".ul-nav");
 let botonPlus = document.querySelector(".boton-plus");
 let form = document.querySelector("form");
@@ -10,6 +18,24 @@ let noHayTarea = document.querySelector(".h1");
 let listaIndex = document.querySelector(".lista-index");
 
 let iconoSeleccionado = null;
+let tiempoSeleccionado = null;
+let arrayTareas = [];
+
+//TIEMPO
+
+function calcularTiempoFaltante() {
+  let tiempoActual = Date.now();
+  let seleccion = parseInt(tiempoSeleccionado);
+
+  tiempoFaltante = seleccion - (tiempoActual % seleccion);
+
+}
+
+// Agregar evento de cambio al elemento select
+contenedorDropdown.addEventListener("change", function (e) {
+  tiempoSeleccionado = e.target.value;
+  let tiempoFaltante = calcularTiempoFaltante();
+});
 
 //FUNCIONES
 
@@ -33,13 +59,9 @@ mostrar(botonPlus, form);
 mostrar(botonPlus, listaIndex);
 mostrar(botonAgregar, form);
 
-
 //funcion para el boton que muestra el form y transforma el boton
 function botonTransform(contenedor) {
-
   botonPlus.addEventListener("click", () => {
-
-    
     const parametro = getComputedStyle(contenedor);
 
     if (parametro.justifyContent == "flex-end") {
@@ -57,27 +79,26 @@ function botonTransform(contenedor) {
 }
 botonTransform(ulNav);
 
-
-
 //funcion evento para guardar la imagen en una variable
 let seleccionIcono = () => {
   let iconos = contenedorIcono.querySelectorAll("img");
 
-
   iconos.forEach((icono) => {
-
-    icono.style.backgroundColor = 'transparent';
-
     icono.addEventListener("click", () => {
       iconoSeleccionado = icono.src;
-      icono.style.backgroundColor = 'var(--verde)';
+      icono.style.backgroundColor = "var(--verde)";
     });
   });
 };
 seleccionIcono();
 
 //funcion que genera la tarea
-function tarea() {
+function tarea(tiempoFaltante) {
+  let segundos = Math.floor((tiempoFaltante / 1000) % 60);
+  let minutos = Math.floor((tiempoFaltante / (1000 * 60)) % 60);
+  let horas = Math.floor((tiempoFaltante / (1000 * 60 * 60)) % 24);
+  let dias = Math.floor(tiempoFaltante / (1000 * 60 * 60 * 24));
+
   let li = document.createElement("li");
   let div = document.createElement("div");
   div.classList.add("tarea");
@@ -87,24 +108,42 @@ function tarea() {
   let h1 = document.createElement("h1");
   h1.textContent = inputTarea.value;
   let p = document.createElement("p");
-  p.textContent = "La tarea debe realizarse en : ";
+  p.textContent = `La tarea debe realizarse en : ${dias} días, ${horas} horas, ${minutos} minutos y ${segundos} segundos`;
   listaIndex.appendChild(li);
   li.appendChild(div);
   div.appendChild(img);
   div.appendChild(div2);
   div2.appendChild(h1);
   div2.appendChild(p);
+
+  // objeto a guardar
+  let objetoTareas = {
+    titulo: inputTarea.value,
+    icono: iconoSeleccionado,
+    tiempoFaltante: {
+      dias: Math.floor(tiempoFaltante / (1000 * 60 * 60 * 24)),
+      horas: Math.floor(tiempoFaltante / (1000 * 60 * 60)) % 24,
+      minutos: Math.floor(tiempoFaltante / (1000 * 60)) % 60,
+      segundos: Math.floor(tiempoFaltante / 1000) % 60
+    }
+  };
+
+  arrayTareas.push(objetoTareas);
+
+  //local storage
+  localStorage.setItem('tareas', JSON.stringify(arrayTareas));
 }
 
 let agregarTarea = () => {
-  
   botonAgregar.addEventListener("click", (e) => {
     e.preventDefault();
-    tarea();
-    reset();
+    if (tiempoSeleccionado !== null || iconoSeleccionado !== null) {
+      tarea(tiempoFaltante);
+      reset();
+    } else {
+      alert("Por favor, selecciona una opción.");
+    }
   });
 };
 
 agregarTarea();
-
-//EVENTOS
